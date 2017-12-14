2b42093f57ca37222ddbe04cfa33b4584e2aff83
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 14:14:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,5 +7,10 @@ def initialize(word) #initializing
   @word = word
 end
 
+def parse #parse method
+    some_word.split.map do |some_word| #seperates the emails
+      some_word.split("") #by finding spaces or commas
+    end.flatten.uniq #and ensures they are unique
+  end
 
 end
