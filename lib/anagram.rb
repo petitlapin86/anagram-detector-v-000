113c65fbe0af10bf3ad95dbc324354a8d50ee3b9
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 14:21:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+attr_accessor :name #creating an attribute accessor
+
+
+def initialize(anagram) #initializing
+  @name= anagram
+end
+
+def match(array)
+   array.select {|x| x.split("").sort == @name.split("").sort}
+ end #split the array into lettors, sort through array elements, checks to see if 2 arrays are equal
+end
