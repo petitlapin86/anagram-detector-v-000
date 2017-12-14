ee7a6cf83947f3931badee2deb0cc58adbdb30e9
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 14:21:47 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ def initialize(anagram) #initializing
   @name= anagram
 end
 
-def match(array) 
+def match(array)
    array.select {|x| x.split("").sort == @name.split("").sort}
  end #split the array into lettors, sort through array elements, checks to see if 2 arrays are equal
 end
